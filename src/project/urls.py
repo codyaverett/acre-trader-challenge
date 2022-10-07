424657dfc15329c877c1accde8f231c5743a6ff1
Cody Averett <codyaverett@gmail.com>
2022-10-07 09:51:23 -0500

    refactor(api): Redirect empty urls to the main api/v1/ route

--- src/project/urls.py
+++ src/project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 import debug_toolbar
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
-    path('todos/', include('todo.urls')),
+    path('', RedirectView.as_view(url='api/v1/', permanent=False)),
+    path('api/v1/', include('todo.urls')),
 ]
