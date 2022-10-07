93207848d1149381cde9c9e34270423445e4c43a
Cody Averett <codyaverett@gmail.com>
2022-10-07 09:05:26 -0500

    refactor: hosting static files from 'cdn' directory now

--- src/project/settings.py
+++ src/project/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'cdn/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'cdn',
 ]
 
 # Default primary key field type
