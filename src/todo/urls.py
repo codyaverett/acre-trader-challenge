fa8e6b3bcb498bd5374bcbe23f8995fc84940133
Cody Averett <codyaverett@gmail.com>
2022-10-07 09:54:43 -0500

    feat(todo app): add django rest_framework routes

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoViewSet)
+router.register(r'todolists', views.TodoListViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('app/', views.app, name='app'),
     path('home/', views.home, name='home'),
 ]
