a62edaa95f6552b31881fa4888db50ac7697f800
Cody Averett <codyaverett@gmail.com>
2022-10-07 01:04:11 -0500

    refactor: use src directory structure for django project and apps

--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
+from os import path, environ
+from dotenv import read_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
+
+    # Load dotenv file from the project root directory
+    dotenv_file = path.join(path.join(path.dirname(__file__), '.env'))
+    read_dotenv(dotenv_file, override=True)
+
+    environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
