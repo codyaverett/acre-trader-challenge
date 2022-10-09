1b884ee52551d192e5efb8fbbb4d69b8878e5404
Cody Averett <codyaverett@gmail.com>
2022-10-08 20:25:54 -0500

    refactor(toolbox): remove toolbox

--- src/todo_site/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from todo_app.models import Todo, TodoList
-
-
-def site(request):
-    return render(request, 'site.html', {
-        'todos': Todo.objects.all(),
-        'todo_lists': TodoList.objects.all(),
-    })
