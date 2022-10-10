89ccf6fc0d8f921aa938e34eb0463282e5e3016b
Cody Averett <codyaverett@gmail.com>
2022-10-09 19:37:51 -0500

    feat(todo_app): override update serializer methods to set updated_at field to the new time

--- src/todo_app/serializers/todo_list_serializer.py
+++ src/todo_app/serializers/todo_list_serializer.py
@@ -1,5 +1,7 @@
-from ..models import Todo, TodoList
 from rest_framework import serializers
+from datetime import datetime
+
+from ..models import TodoList
 
 
 class TodoListSerializer(serializers.ModelSerializer):
@@ -7,3 +9,9 @@ class TodoListSerializer(serializers.ModelSerializer):
         model = TodoList
         fields = ('id', 'title', 'todo', 'updated_at', 'created_by')
         lookup_field = 'id'
+
+    # Update a todoList updated_at field when it is updated
+    def update(self, instance, validated_data):
+        instance.updated_at = datetime.now()
+        instance.save()
+        return super().update(instance, validated_data)
--- src/todo_app/serializers/todo_serializer.py
+++ src/todo_app/serializers/todo_serializer.py
@@ -1,5 +1,7 @@
-from ..models import Todo
 from rest_framework import serializers
+from datetime import datetime
+
+from ..models import Todo
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -8,3 +10,9 @@ class TodoSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'detail', 'created_at',
                   'completed_at', 'updated_at', 'created_by', 'important')
         lookup_field = 'id'
+
+    # Update a todo's updated_at field when it is updated
+    def update(self, instance, validated_data):
+        instance.updated_at = datetime.now()
+        instance.save()
+        return super().update(instance, validated_data)
